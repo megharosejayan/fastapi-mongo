046b91045b8444b78212a410c9447e7d7608e889
megharosejayan <megharose15@gmail.com>
2022-08-09 14:49:28 +0900

    create and push student to db

--- app/server/app.py
+++ app/server/app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
-app = FastAPI()
 
+app = FastAPI()
+from server.routes.student import router as StudentRouter
+app.include_router(StudentRouter, tags=["Student"], prefix="/student")
 
 @app.get("/", tags=["Root"])
 async def read_root():
